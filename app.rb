b0eeb789454b8ad34ef823b338e6dfdabb1c14d4
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-03-02 18:39:58 +0000

    New method and more stuff in search two in app.rb

--- app.rb
+++ app.rb
@@ -5,6 +5,53 @@ bad_string = false
 bad_integer = false
 
 unified_id = 0
+generation = 0
+
+display_name = ""
+
+serebii_web_address = ""
+
+def serebii_web_address_generator(serebii_name)
+  if generation == 1
+    
+    serebii_web_address = “https://www.serebii.net/pokedex/{serebii_name}.shtml”
+
+  elsif generation == 2
+
+    serebii_web_address = “https://www.serebii.net/pokedex-gs/{serebii_name}.shtml”
+
+  elsif generation == 3
+
+    serebii_web_address = “https://www.serebii.net/pokedex-rs/{serebii_name}.shtml”
+
+  elsif generation == 4
+
+    serebii_web_address = “https://www.serebii.net/pokedex-dp/{serebii_name}.shtml”
+
+  elsif generation == 5
+
+    serebii_web_address = “https://www.serebii.net/pokedex-bw/{serebii_name}.shtml”
+
+  elsif generation == 6
+
+    serebii_web_address = “https://www.serebii.net/pokedex-xy/{serebii_name}.shtml”
+
+  elsif generation == 7
+
+    serebii_web_address = “https://www.serebii.net/pokedex-sm/{serebii_name}.shtml”
+
+  elsif generation == 8
+
+    serebii_web_address = “https://www.serebii.net/pokedex-swsh/{serebii_name}”
+
+  elsif generation == 9
+
+    serebii_web_address = “https://www.serebii.net/pokedex-sv/{serebii_name}“
+
+  else
+    pp "You made a big mistake with the generation checker! Go fix it!"
+  end
+end
 
 get("/") do
   redirect("/search_one")
@@ -38,10 +85,16 @@ get("/search_two") do
     else
       unified_id = @formatting_id
     end
+    api_url = "https://pokeapi.co/api/v2/pokemon/#{unified_id}"
+    @raw_respnose = HTTP.get(api_url)
+    @raw_string = @raw_response.to_s
+    @parsed_data = JSON.parse(@raw_string)
+    display_name = @parsed_data.fetch("name").capitalize
   end
   erb(:search_two)
 end
 
 get("/results") do
+  
   erb(:search_results)
 end
