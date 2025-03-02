de6ae7a6055abb58d81c995b5ba9d89caecfa6d5
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-03-02 21:58:05 +0000

    Large in app.rb, smaller changes in erb files

--- app.rb
+++ app.rb
@@ -1,26 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+
 enable :sessions
 
-unified_id = 0
-generation = 0
+# unified_id = 0
+# generation = 0
 
-display_name = ""
-working_name = ""
+# display_name = ""
+# working_name = ""
 
-official_name = ""
-bulba_name = ""
-serebii_net_name = ""
+# official_name = ""
+# bulba_name = ""
+# serebii_net_name = ""
 
-official_web_address = ""
-bulba_web_address = ""
-serebii_web_address = ""
+# official_web_address = ""
+# bulba_web_address = ""
+# serebii_web_address = ""
 
 def serebii_name_generator(working_name)
-  if generation == 1 | 2 | 3 | 4 | 5 | 6 | 7
-    serebii_name = unified_id.to_s # Figure out how to add leading zeroes to a string
-  elsif generation == 8 | 9
+  if generation <= 7
+    serebii_name = unified_id.to_s.rjust(3, "0") # Figure out how to add leading zeroes to a string
+  elsif generation > 7
     serebii_name = params.fetch("name")
   end
 end
@@ -60,47 +62,59 @@ get("/search_one") do
 end
 
 get("/search_two") do
+  search_id = params.fetch("search_id", "")
+  search_name = params.fetch("search_name", "")
 
-  if params.fetch("search_id") == "" # Do parameters initialize as blank?
-    formatted_string = params.fetch("search_name").gsub!(/[Ã¨Ã©ÃªÃ«Ä?Ä?Ä?Ä?Ä?]/u, 'e').gsub('.','').gsub(' ','-')
+  if not search_name.empty?
+    # Searching by name
+    formatted_string = search_name.gsub('é', 'e').gsub('. ','').gsub(' ','-').gsub(': ', '-')
     api_url = "https://pokeapi.co/api/v2/pokemon/#{formatted_string}"
-    @raw_respnose = HTTP.get(api_url)
-    @raw_string = @raw_response.to_s
-    @parsed_data = JSON.parse(@raw_string)
-    @id = @parsed_data.fetch("id").to_i
-    if @id == "" # I have to do this check somehow but I am not sure how to check for this
+    raw_response = HTTP.get(api_url)
+    parsed_data = JSON.parse(raw_response.to_s)
+
+    @id = parsed_data.fetch("id").to_i
+
+    if @id.to_s.empty?
       session.store(:bad_string, true)
       redirect("/search_one")
     else
-      unified_id = @id
+      session.store(:unified_id, @id)
+      working_name = parsed_data.fetch("name")
+      session.store(:display_name, working_name.capitalize)
     end
-  end
-  if params.fetch("search_name") == "" # Again, I should check if this works to check that this parameter hasn't been initialized
+
+  elsif not search_id.empty? 
+    # Searching by ID
     @formatting_id = @search_id.to_i
-    if @formatting_id > 1025 | @formatting_id < 1
+    if @formatting_id > 1025 || @formatting_id < 1
       session.store(:bad_integer, true)
       redirect("/search_one")
     else
-      unified_id = @formatting_id
+      session.store(:unified_id, @formatting_id)
     end
     api_url = "https://pokeapi.co/api/v2/pokemon/#{unified_id}"
-    @raw_respnose = HTTP.get(api_url)
-    @raw_string = @raw_response.to_s
-    @parsed_data = JSON.parse(@raw_string)
+    raw_response = HTTP.get(api_url)
+    parsed_data = JSON.parse(raw_response.to_s)
 
-    working_name = @parsed_data.fetch("name")
-    display_name = working_name.capitalize
+    working_name = parsed_data.fetch("name")
+    session.store(:display_name, working_name.capitalize)
+    session.store(:working_name, working_name)
+  elsif search_name.empty? && search_id.empty?
+    redirect("/search_one")
   end
+
   erb(:search_two)
 end
 
 get("/results") do
-  bulba_name = working_name.capitalize
-  official_name = working_name
+  bulba_name = session.fetch(:working_name).capitalize
+  official_name = session.fetch(:working_name)
 
-  official_web_address = "https://www.pokemon.com/us/pokedex/{official_name}"
+  @official_web_address = "https://www.pokemon.com/us/pokedex/{official_name}"
 
-  bulba_web_address = "https://bulbapedia.bulbagarden.net/wiki/{bulba_name}_(Pokémon)"
+  @bulba_web_address = "https://bulbapedia.bulbagarden.net/wiki/{bulba_name}_(Pokémon)"
+
+
 
   erb(:search_results)
 
