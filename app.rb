9b32318037c5e205ed4701f8bc46d2abe5f4afb8
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-03-02 17:39:19 +0000

    Set up main page redirect

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  redirect("/search_one")
+end
+
+get("/search_one") do
   erb(:search_one)
 end
 
