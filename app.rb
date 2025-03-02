31d235c28db526cdde988a260623caad7b4f2ef0
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-03-02 02:21:46 +0000

    Wrote erb(:) for each path in app.rb

--- app.rb
+++ app.rb
@@ -6,12 +6,13 @@ get("/") do
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+  erb(:search_one)
 end
 
 get("/search_details") do
-
+  erb(:search_two)
 end
 
 get("/results") do
-  
+  erb(:search_results)
 end
